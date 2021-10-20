86ffe06d6afd05c41e842e8b7a638db2591d31f2
seungchulpark <parkseungchul@gmail.com>
2021-10-20 13:49:38 +0900

    add j103

---
+++ j103/src/main/java/shutup/code/j003/J003Application.java
@@ -0,0 +1,13 @@
+package shutup.code.j003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J003Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J003Application.class, args);
+	}
+
+}
---
+++ j103/src/test/java/shutup/code/j003/J003ApplicationTests.java
@@ -0,0 +1,13 @@
+package shutup.code.j003;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J003ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
